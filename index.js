5f139417d0736357b5c8a93997cf8347625bcc89
Rahul IB <166328737+RahulIB5@users.noreply.github.com>
2025-01-09 18:34:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+randomNumber1 = Math.ceil(Math.random()*6);
+randomNumber2 = Math.ceil(Math.random()*6);
+
+document.querySelector(".img1").setAttribute("src","./images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src","./images/dice" + randomNumber2 + ".png");
+
+document.querySelector("h1").innerHTML = "Dyumm";
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
